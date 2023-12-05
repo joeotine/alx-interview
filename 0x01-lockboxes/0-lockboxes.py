9bbfb775c07f7ed6781c7d36c9cbcc40e3bfc094
Joseph Otine <joeotine@gmail.com>
2023-12-04 21:59:09 -0800

    task completed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """
-This module provides a function that determines if all
-boxes in a given list can be opened.
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+More info:
+- Prototype: def canUnlockAll(boxes)
+- boxes is a list of lists
+- A key with the same number as a box opens that box
+- You can assume all keys will be positive integers
+- The first box boxes[0] is unlocked
+- Return True if all boxes can be opened, else return False
 """
 
 
 def canUnlockAll(boxes):
     """
-    This is a function that takes a list of lists and returns a boolean indicating
-        whether all boxes in the list can be opened. A key with the same
-        number as a box opens that box. You can assume all keys will be
-        positive integers. There can be keys that do not have boxes.
-        The first box boxes[0] is unlocked.
-
-    Parameters:
-    boxes (List[List[int]]): The list of lists representing the boxes
-        and their keys.
-
-    Returns:
-    bool: True if all boxes can be opened, else False.
+    Description:
+    Write a method that determines if all the boxes can be opened
+    Arguments:
+    boxes --> List of Lists, it contains the boxes with keys
+    Reurn boolean
+    Variables:
+    myKeys --> List, Store the number keys to open boxes
+    key --> integer, key of the myKeys
+    boxKey --> iinteger, key inside of an specific box
     """
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
+    return False
